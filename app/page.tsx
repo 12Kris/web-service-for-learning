803b6ba499744480781f8915c1f1caaa731ca5b8
Mykhailo <mn508678@gmail.com>
2024-11-14 12:30:26 +0100

    add button to test db and loader

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 import { Jumbotron } from "@/components/jumbotron";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className="">
       <Jumbotron title="Welcome to Our Amazing App" description="Discover the power of innovation and creativity" buttonText="Get Started" buttonLink="/login" />
+      <div className="flex flex-col items-center mt-10 justify-center">
+        <Link href="/courses">
+          <Button>Courses (To Check DB Connection)</Button>
+        </Link>
+      </div>
     </div>
   );
 }
