9e4cfb5bbb9c0aa84b475371fdb4b3b1b3fc62f4
Mykhailo <mn508678@gmail.com>
2024-11-21 18:13:23 +0100

    update Jumbotron according to design

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,9 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="">
-      <Jumbotron title="Welcome to Our Amazing App" description="Discover the power of innovation and creativity" buttonText="Get Started" buttonLink="/login" />
+    <div className="bg-[--background]">
+      <Jumbotron title="Unlock Your Potential: Learn New Skills, Shape Your Future" description="Discover a unique approach to learning that fits your lifestyle. Our courses are designed to help you grow, succeed, and reach new heights in your career."
+       primaryButtonText="Get Started" primaryButtonLink="/login" secondaryButtonText="Learn More" secondaryButtonLink="/about" />
       <div className="flex flex-col items-center mt-10 justify-center">
         <Link href="/courses">
           <Button>Courses (To Check DB Connection)</Button>
