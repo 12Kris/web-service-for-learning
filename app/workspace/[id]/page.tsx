700c4162680823d177be9804ec04a292d1b71a95
Bogdan Bikbulatov <batmansila1234@gmail.com>
2024-11-26 13:38:39 +0200

    moved singe page

---
+++ app/workspace/[id]/page.tsx
@@ -0,0 +1,79 @@
+"use client";
+import { useEffect, useState } from "react";
+import { getCourseById, addCourseToUser, isCourseAddedToUser } from "@/lib/courses/actions";
+import { Button } from "@/components/ui/button";
+import { getUser } from "@/lib/authActions";
+import {use} from 'react';
+
+interface Course {
+    id: string;
+    title: string;
+    description: string;
+    instructor_name: string;
+}
+
+export default function CourseDetailPage({ params }: { params: Promise<{ id: string }> }) {
+    const [course, setCourse] = useState<Course | null>(null);
+    const [isCourseAdded, setIsCourseAdded] = useState(false);
+    const [userId, setUserId] = useState<string | null>(null);
+    const { id } = use(params);
+
+    useEffect(() => {
+        async function fetchCourse() {
+            if (!id) return;
+            const courseData = await getCourseById(id);
+            setCourse(courseData);
+        }
+
+        fetchCourse();
+    }, [id]);
+
+    useEffect(() => {
+        async function fetchUser() {
+            const user = await getUser();
+            if (user) {
+                setUserId(user.id);
+            }
+        }
+
+        fetchUser();
+    }, []);
+
+    useEffect(() => {
+        async function checkCourse() {
+            if (userId && id) {
+                const result = await isCourseAddedToUser(userId, id);
+                console.log(result)
+                setIsCourseAdded(result);
+            }
+        }
+
+        checkCourse();
+    }, [userId, id]);
+
+    if (!course) {
+        return <div>Loading...</div>;
+    }
+
+    const handleAddCourse = async () => {
+        if (!userId || !course.id) return;
+        await addCourseToUser(course.id, userId);
+        alert("Course added to your account!");
+        setIsCourseAdded(true);
+    };
+
+    return (
+        <div className="container mx-auto py-10 px-4">
+            <h1 className="text-3xl font-bold mb-6">{course.title}</h1>
+            <div className="mb-6">{course.description}</div>
+            <div>
+                <span className="font-bold">Instructor:</span> {course.instructor_name}
+            </div>
+            <div className="my-4">
+                <Button onClick={handleAddCourse} disabled={isCourseAdded}>
+                    {isCourseAdded ? "Course Added" : "Add this course to my account"}
+                </Button>
+            </div>
+        </div>
+    );
+}
